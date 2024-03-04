06f6f16641ce6a4f719ee19bbb7f7c7d263b02e3
Mohammed Thasleem MK <mohammedthasleemmk6604@gmail.com>
2024-03-04 16:35:41 +0530

    First C# program

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Model
+{
+    class Parent:Sample.Sample
+    {
+        public void sum(int num1,int num2)
+        {
+            System.Console.WriteLine($"The sum is: {num1+num2}");
+        }
+    }
+    class Programs:Parent
+    {
+        public static void Main(string[] args)
+        {
+            Parent Obj1=new Parent();
+            Programs Obj2=new Programs();
+            Obj2.sum(10,12);
+        }
+    }
+}
---
+++ Sample.cs
@@ -0,0 +1,12 @@
+using System;
+namespace Sample
+{
+   public class Sample
+    {
+        public Sample()
+        {
+            System.Console.WriteLine("Hello");
+        }
+        
+    }
+}
